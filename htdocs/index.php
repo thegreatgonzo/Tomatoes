1303d0b607b0711cbe890c769c9fac3ed025cb78
Tony Kennick <tony.kennick@gmail.com>
2018-01-28 15:20:51 +0000

    Initial add

---
+++ htdocs/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Tomatoes</title>
+</head>
+
+<body>
+<img src="http://toms.pint.org.uk/psvg/jar.svg" height="300"><br clear="all">
+<img src="http://toms.pint.org.uk/psvg/tomato.svg" height="300">
+</body>
+
+</html>
